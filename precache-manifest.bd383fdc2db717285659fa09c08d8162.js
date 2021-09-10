919cafc6c79bae6f387f43d968d72c9a85d3f1b4
yangmm1324 <yangmm1324@gmail.com>
2021-09-10 12:27:28 -0700

    Updates

---
+++ precache-manifest.bd383fdc2db717285659fa09c08d8162.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5053b951690a5fb48d5152b9f2d486",
+    "url": "/Galapago/index.html"
+  },
+  {
+    "revision": "5778a3f5d5bf8d181677",
+    "url": "/Galapago/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "cfffea146ee4c55e17e1",
+    "url": "/Galapago/static/css/main.213240d6.chunk.css"
+  },
+  {
+    "revision": "5778a3f5d5bf8d181677",
+    "url": "/Galapago/static/js/2.32782535.chunk.js"
+  },
+  {
+    "revision": "859d82b73e973d1fc0746ea1b5ac4cf7",
+    "url": "/Galapago/static/js/2.32782535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfffea146ee4c55e17e1",
+    "url": "/Galapago/static/js/main.26dbb5c0.chunk.js"
+  },
+  {
+    "revision": "a3208fd2acded7a2465c",
+    "url": "/Galapago/static/js/runtime-main.b9ad8461.js"
+  },
+  {
+    "revision": "ff10bf3a26597d896a11657da2ac3712",
+    "url": "/Galapago/static/media/logo.ff10bf3a.png"
+  }
+]);
